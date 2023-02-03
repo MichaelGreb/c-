5158bfc44890d4a742ccddc325092a5955b017f8
oneboss <123@example.com>
2023-02-03 20:19:32 +0300

    add task_001

---
+++ task_001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Кавдрат заданного числа
+
+Console.Clear();
+Console.Write("Введите целое число:");
+int Number = int.Parse(Console.ReadLine()); // int.Parse перевод введенного пользователем значения из символного в числовой
+int sqr = Number * Number;
+Console.WriteLine("Квадрат числа " + Number + " равен = " + sqr);
+
