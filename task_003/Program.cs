2a014092ab1b6ad5a3e610c552e874d36c31e5b2
oneboss <123@example.com>
2023-02-03 23:29:07 +0300

    add 003

---
+++ task_003/Program.cs
@@ -0,0 +1,40 @@
+﻿// Дни недели
+Console.Clear();
+Console.Write("Введите день недели: ");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+    {
+        Console.Write("понедельник");
+    }
+if(number == 2)
+    {
+        Console.Write("вторник");
+    }
+if(number == 3)
+    {
+        Console.Write("среда");
+    }
+if(number == 4)
+    {
+        Console.Write("четверг");
+    }
+if(number == 5)
+    {
+        Console.Write("пятница");
+    }
+if(number == 6)
+    {
+        Console.Write("суббота");
+    }
+if(number == 7)
+    {
+        Console.Write("воскресенье");
+    }
+if(number > 7)
+    {
+        Console.Write("значение неверно");
+    }
+    if(number < 1)
+    {
+        Console.Write("значение неверно");
+    }
