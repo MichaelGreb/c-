3c4e925dddba26ae423405e4f3dfc8b7b20588fe
oneboss <123@example.com>
2023-02-04 11:51:28 +0300

    add 004

---
+++ task_004/Program.cs
@@ -0,0 +1,12 @@
+﻿// на вход принимает одно число (N), а на выходе показывает все целые числа 
+// в промежутке от -N до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int P = -N;
+while(P <= N)
+    {
+        Console.Write(P + " ");
+        P++;
+    }
