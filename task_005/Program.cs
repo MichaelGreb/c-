c56421460a809621dd639a71c2165df764babcc3
oneboss <123@example.com>
2023-02-04 12:36:51 +0300

    add 005

---
+++ task_005/Program.cs
@@ -0,0 +1,8 @@
+﻿// принимает на вход трёхзначное число и на выходе показывает 
+//последнюю цифру этого числа
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number%10);
+
