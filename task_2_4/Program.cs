6ca129aec642c15ca9982e411e257a152ce5e32d
oneboss <123@example.com>
2023-02-05 19:41:39 +0300

    add 2_4

---
+++ task_2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = int.Parse(Console.ReadLine());
+if((n1 % 7 == 0) & (n1 % 23 ==0))
+{
+    Console.WriteLine($"{n1} -> да");
+}
+else
+{
+    Console.WriteLine($"{n1} -> нет");
+}
