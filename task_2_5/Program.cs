3d89399bb477dfbb89ecbe3eaed086a2df7f4bba
oneboss <123@example.com>
2023-02-05 19:49:46 +0300

    add 2_5

---
+++ task_2_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = int.Parse(Console.ReadLine());
+if((n1 * n1 == n2) || (n2 % n2 == n1))
+{
+    Console.WriteLine($"{n1},{n2} -> да");
+}
+else
+{
+    Console.WriteLine($"{n1},{n2} -> нет");
+}
+
+
