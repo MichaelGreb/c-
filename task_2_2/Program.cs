15dea51b0513862be27927ffbd607e10470f8fae
oneboss <123@example.com>
2023-02-05 18:27:41 +0300

    add 2_2

---
+++ task_2_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Console.Clear();
+int num = new Random().Next(100,1000);
+int a1 = num / 100;
+int a2 = num %10;
+int result = a1 * 10 + a2;
+Console.WriteLine($"{num} -> {result}");
