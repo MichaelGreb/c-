4856d892bf74efce6b17d832379af7f1bb974f8c
oneboss <123@example.com>
2023-02-12 21:38:59 +0300

    task_4_1

---
+++ task_4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 24: принимает число А и выдает сумму чисел от 1 до А.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма циифр = {GemSumNums(num)} ");
+
+int GemSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+        {
+            sum += number;
+            number--;
+        }
+    return sum;
+}
