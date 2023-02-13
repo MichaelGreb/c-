a07ed63123efa9a2e062cedcba4f17185eb9aae6
oneboss <123@example.com>
2023-02-13 13:49:57 +0300

    task_4_2

---
+++ task_4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает число и выдает количество цифр в числе
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = 1;
+while(num>9)
+{
+    num=num/10;
+    count=count+1;
+}
+Console.WriteLine($"количество цифр = {count} ");
